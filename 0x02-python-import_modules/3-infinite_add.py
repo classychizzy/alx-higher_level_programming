469ec475e65ce71cad8330b1c71c14a8c925caee
classychizzy <chizobachukwuoma@gmail.com>
2022-06-02 21:44:58 +0000

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
+    """function that prints the addition of command arguments"""
     import sys
     result = 0
-    for i in range(len(sys.argv)):
-        result += int(sys.argv[i])
+    for i in range(len(sys.argv) - 1):
+        result += int(sys.argv[i + 1])
         print("{}".format(result))
