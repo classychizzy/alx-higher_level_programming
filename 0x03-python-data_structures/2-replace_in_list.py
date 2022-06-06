70b7c1c6bb5639551ca3096dcc6d396bb5000aec
classychizzy <chizobachukwuoma@gmail.com>
2022-06-06 07:51:28 +0000

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,6 +2,7 @@
 def replace_in_list(my_list, idx, new_element):
     for i in range(len(my_list)):
         if idx < 0 or idx > (len(my_list) - 1):
+            new_element = None
             return my_list
         elif idx == my_list[i]:
             my_list[i] = new_element
