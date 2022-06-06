7ced1873aa95a651823c9a591ffbfa25a411acbd
classychizzy <chizobachukwuoma@gmail.com>
2022-06-06 23:01:40 +0000

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        column = row
+        for i in column:
+            print("{:d}".format(i), end =" " if column != row[-1] else "")
+        print()
