880ca917b6bfeca60c0f0c1239b640d215584333
classychizzy <chizobachukwuoma@gmail.com>
2022-06-07 11:54:43 +0000

    task8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        s_tuple = 0, None
+    else:
+        s_tuple = len(sentence), sentence[0]
+        return s_tuple
