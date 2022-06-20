19b3499ffb9625da90114090f299fcc56d776247
classychizzy <chizobachukwuoma@gmail.com>
2022-06-20 21:00:41 +0000

    task3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except (ZeroDivisionError, FloatingPointError):
+        div = None
+    finally:
+        print("Insideresult:{}".format(div), end="")
+    return div
