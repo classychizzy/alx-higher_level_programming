5eccf518c07e68f53f7f00d37b4a8cc9d897fed6
classychizzy <chizobachukwuoma@gmail.com>
2022-06-20 21:32:06 +0000

    task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,8 +12,8 @@ def list_division(my_list_1, my_list_2, list_length):
             my_list_2[i] = 0
             print("division by 0")
         except IndexError:
-            tempResult = 0
-            print("out of range")
+            if len(my_list_1) < list_length or len(my_list_2) < list_length:
+                print("out of range")
         finally:
             pass
         length.append(tempResult)
