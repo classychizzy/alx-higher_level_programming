e2f00ec0cb0934551b3e3229329506ca21965605
classychizzy <chizobachukwuoma@gmail.com>
2022-07-04 19:30:08 +0000

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" a function that returns a list of available
+ methods and attributes
+"""
+
+
+def lookup(obj):
+    """ return a list object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+##introduction to inheritance and subclasses in python
