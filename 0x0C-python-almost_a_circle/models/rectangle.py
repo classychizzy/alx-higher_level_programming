73d02af9e472899a6c024d329d1aa099b64cf90d
classychizzy <chizobachukwuoma@gmail.com>
2022-07-12 21:28:42 +0000

    fixed bug in rectangles and added tasks 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,3 +80,63 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("x must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Computes the area of this rectangle.
+        Returns: area
+        int: The area of this rectangle.
+        """
+        return self.width * self.height
+
+    def display(self):
+        """Prints a text representation of this rectangle.
+        """
+        h_off = ' ' * self.x
+        h_val = '#' * self.width
+        print('\n' * self.y, end='')
+        print('{:s}{:s}\n'.format(h_off, h_val) * self.height, end='')
+
+    def __str__(self):
+        """Creates a string representation of this polygon.
+        Returns:
+            str: A string representation of this polygon.
+        """
+        parts = (
+            self.id,
+            self.x,
+            self.y,
+            self.width,
+            self.height
+        )
+        res = '[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}'.format(
+            parts[0], parts[1], parts[2], parts[3], parts[4]
+        )
+        return res
+
+    def update(self, *args, **kwargs):
+        """Updates the attributes of this polygon.
+        Args:
+            args (tuple): A tuple of non-keyword arguments.
+            kwargs (dict): A dictionary of keyword arguments.
+        """
+        attrs = ('id', 'width', 'height', 'x', 'y')
+        for key, val in zip(attrs, args):
+            setattr(self, key, val)
+        if (type(args) is None or len(args) == 0) and (type(kwargs) is dict):
+            for key, val in kwargs.items():
+                if key in attrs:
+                    setattr(self, key, val)
+
+    def to_dictionary(self):
+        """Creates a dictionary representation of this polygon.
+        Returns:
+            dict: A dictionary representation of this polygon.
+        """
+        res = {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
+        return res
