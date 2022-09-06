26bb85df152d8dc806639ca110cab7c5eb388945
classychizzy <chizobachukwuoma@gmail.com>
2022-09-06 22:08:49 +0000

    task 3 - 9

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -14,6 +14,15 @@ class Rectangle {
       this.height = h;
     }
   }
+
+  /**
+   * Prints this Rectangle with the character 'X'.
+   */
+  print () {
+    const row = new Array(this.width).fill('X', 0, this.width);
+    const rows = new Array(this.height).fill(row.join(''), 0, this.height);
+    console.log(rows.join('\n'));
+  }
 }
 
 module.exports = Rectangle;
