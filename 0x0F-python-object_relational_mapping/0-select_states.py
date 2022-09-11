06163f50fe8a8541fad1b3b6bce4fce7c5c39712
classychizzy <chizobachukwuoma@gmail.com>
2022-09-11 20:33:29 +0000

    task3

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,27 +4,28 @@
 """a script that shows all states in the database
 hbtn_0e_0_usa"""
 
+import sys
 import MySQLdb
 
-
-"""connection to the database"""
-mydb = MySQLdb.connect(
+if __name__ == "__main__":
+    """connection to the database"""
+    mydb = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user="classychizzy",
-        password="@Chizzy2636",
-        db="hbtn_0e_0_usa"
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3]
         )
 
-"""create an instance of 'cursor' class
-which is used to execute SQL statements
-in python """
-cursor = mydb.cursor()
-""" to show all columns in the database"""
-cursor.execute('SELECT * FROM states ORDER BY id ASC;')
-"""command to fetch all rows"""
-results = cursor.fetchall()
-for result in results:
-    print(result)
+    """create an instance of 'cursor' class
+    which is used to execute SQL statements
+    in python """
+    cursor = mydb.cursor()
+    """ to show all columns in the database"""
+    cursor.execute('SELECT * FROM states ORDER BY id ASC;')
+    """command to fetch all rows"""
+    results = cursor.fetchall()
+    for result in results:
+        print(result)
 
-mydb.close()
+    mydb.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,9 +8,9 @@ import MySQLdb
 mydb = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user="classychizzy",
-        passwd="@Chizzy2636",
-        db="hbtn_0e_0_usa"
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
         )
 
 """get cursor object"""
